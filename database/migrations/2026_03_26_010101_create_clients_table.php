3caedeb85cc9313f1463119b899823662bb5ae34
Thais Lara <thaiscostalara@gmail.com>
2026-03-26 22:03:44 -0300

    feat: adiciona migration para criação da tabela clientes

---
+++ database/migrations/2026_03_26_010101_create_clients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+        $table->id();
+        $table->string('nome');
+        $table->string('documento', 14)->unique();
+        $table->string('documento_tipo', 4);
+        $table->string('email')->nullable();
+        $table->string('telefone', 20)->nullable();
+        $table->text('endereco')->nullable();
+        $table->date('data_nascimento')->nullable();
+        $table->timestamp('data_inclusao')->useCurrent();
+
+        // Vesão do laravel não suporta
+        //$table->check("documento_tipo IN ('CPF', 'CNPJ')");
+    });
+
+        DB::statement(
+            "ALTER TABLE clientes
+            ADD CONSTRAINT check_documento_tipo
+            CHECK (documento_tipo IN ('CPF', 'CNPJ'));
+        ");
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
