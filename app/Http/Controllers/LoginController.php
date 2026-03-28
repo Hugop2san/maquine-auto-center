f8b2edb7143f7d489123d1d4001affc35f43b171
Thais Lara <thaiscostalara@gmail.com>
2026-03-28 00:07:09 -0300

    style: formatar código usando Laravel Pint

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,21 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-
 
 class LoginController extends Controller
 {
     public function login(LoginRequest $request)
     {
 
-
-
-    
         if (! Auth::attempt($request->only('email', 'password'))) {
             return response()->json([
                 'status' => false,
